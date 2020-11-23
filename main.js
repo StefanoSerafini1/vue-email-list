58de929947b729e81ab1b31ffdaa0e4fcbc7e188
StefanoSerafini <stefanoserafini7@gmail.com>
2020-11-23 17:58:59 +0100

    aggiunta commenti

--- main.js
+++ main.js
@@ -4,17 +4,18 @@ const app = new Vue({
     emails: [],
   },
   created() {
-    this.generazioneMail();
+    this.generazioneMail();//richiamo funz di generazione con this funzionante in vue all'interno del created
   },
   methods: {
+    //funzione di generazione di 10 mail num impostato dall variabile num mail
     generazioneMail() {
       this.emails=[];
       let numMail=10;
-      for (let i = 0; i < numMail; i++) {
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+      for (let i = 0; i < numMail; i++) {//ciclo for x10
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')//link per richiamo mail random
              .then(response => {
-              this.emails.push(response.data.response)
-              console.log(response);
+              this.emails.push(response.data.response)//push delle mail nel pacchetto all'interno del array mails
+              console.log(response);//console log per controllo pacchetti
               })
 
       }
